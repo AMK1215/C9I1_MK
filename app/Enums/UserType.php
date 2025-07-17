ead0a276f71f84b41593cd7cd7656592056a35ea
AMK1215 <aungmyokhaing1215@gmail.com>
2025-07-17 09:19:32 +0630

    production game list

--- app/Enums/UserType.php
+++ app/Enums/UserType.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum UserType: int
 {
     case Owner = 10;
+    case Master = 11;
     case Agent = 20;
     case SubAgent = 30;
     case Player = 40;
@@ -14,30 +15,33 @@ enum UserType: int
     {
         return match ($type) {
             self::Owner => 1,
-            self::Agent => 2,
-            self::SubAgent => 3,
-            self::Player => 4,
-            self::SystemWallet => 5,
+            self::Master => 2,
+            self::Agent => 3,
+            self::SubAgent => 4,
+            self::Player => 5,
+            self::SystemWallet => 6,
         };
     }
 
     public static function childUserType(UserType $type): UserType
     {
         return match ($type) {
-            self::Owner => self::Agent,
+            self::Owner => self::Master,
+            self::Master => self::Agent,
             self::Agent => self::SubAgent,
             self::SubAgent => self::Player,
-            self::Player, self::SystemWallet => self::Player,
+            self::Player, self::SystemWallet => self::Player, 
         };
     }
 
     public static function canHaveChild(UserType $parent, UserType $child): bool
     {
         return match ($parent) {
-            self::Owner => $child === self::Agent,
+            self::Owner => $child === self::Master,
+            self::Master => $child === self::Agent,
             self::Agent => $child === self::SubAgent || $child === self::Player,
             self::SubAgent => $child === self::Player,
-            self::Player, self::SystemWallet => false,
+            self::Player, self::SystemWallet => false, 
         };
     }
 }
